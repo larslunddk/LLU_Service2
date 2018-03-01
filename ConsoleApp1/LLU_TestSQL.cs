80fbf9556f3f893e4937841af0504667b51e68f5
larslund66 <llu@lundpartner.dk>
2018-03-01 15:34:32 +0100

    Finally works but takes too long time

--- ConsoleApp1/LLU_TestSQL.cs
+++ ConsoleApp1/LLU_TestSQL.cs
@@ -5,31 +5,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace NSLLU_TestSQL
 {
     class LLU_TestSQL
     {
         static void Main()
         {
             string ret = "";
-            string sqlConnectionString = "Server= localhost; Database= LLUTEST; Integrated Security = SSPI; ";
+            string sqlConnectionString = "Server=localhost; Database=LLUTEST; Integrated Security = SSPI; ";
             SqlConnection cnn = new SqlConnection(sqlConnectionString);
             SqlCommand sqlCommand;
             SqlDataReader sqlDatareader;
             try
             {
                 cnn.Open();
-                string sql = "select count(*) from CU8";
+                string sql = "select count(*) from LLUTEST.dbo.CU8";
                 sqlCommand = new SqlCommand(sql, cnn);
                 sqlDatareader = sqlCommand.ExecuteReader();
                 while (sqlDatareader.Read())
                 {
                     string CountStr = sqlDatareader.GetValue(0).ToString();
-                    Int64 CountInt = sqlDatareader.GetInt64(0);
+                    //Int64 CountInt = sqlDatareader.GetInt64(0);
+                    Int64 CountInt = Convert.ToInt64(CountStr);
                     Int64 CountExpected = 136276;
+
                     if (CountInt != CountExpected)
                     {
-                        
                         ret = string.Format("Error counting  Records {0} --> not equal to {1} (SQLConnectionstring:{2})", CountInt, CountExpected, sqlConnectionString);
                     }
                     else
